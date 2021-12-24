190ae9ea9f047c85befea4c3d74b9f288dcdbf22
Matthew Fenwick <mfenwick100@gmail.com>
2021-12-24 15:37:15 -0500

    create examples script

---
+++ go/examples.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -xv
+set -euo pipefail
+
+
+# compare
+go run cmd/json-finder/main.go compare \
+  --version 1.18.19,1.23.0 \
+  --type CustomResourceDefinition > compare-crd.txt
+
+git diff --no-index old-compare-crd.txt compare-crd.txt
+
+
+# explain
+go run cmd/json-finder/main.go explain \
+  --version 1.18.19 \
+  --type CustomResourceDefinition > explain-crd.txt
+
+git diff --no-index old-explain-crd.txt explain-crd.txt
+
+
+# parse
+go run cmd/json-finder/main.go parse --version 1.18.19
