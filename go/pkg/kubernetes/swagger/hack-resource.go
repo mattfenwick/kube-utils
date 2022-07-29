0923e28fb2bbccba340275a213966cc12cb9a170
Matthew Fenwick <mfenwick100@gmail.com>
2022-07-28 21:13:46 -0400

    clean up dead code

--- go/pkg/kubernetes/swagger/hack-resource.go
+++
@@ -1,6 +0,0 @@
-package swagger
-
-func HackExplainResource(args *ExplainResourceArgs) {
-	spec := HackMustReadSwaggerSpecFromGithub(MustVersion(args.Version))
-	spec.ResolveStructure(args)
-}
--- go/pkg/kubernetes/swagger/kubespec.go
+++ go/pkg/kubernetes/swagger/kubespec.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mattfenwick/collections/pkg/set"
 	"github.com/mattfenwick/collections/pkg/slice"
 	"github.com/mattfenwick/kube-utils/go/pkg/utils"
+	"github.com/olekukonko/tablewriter"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/exp/maps"
@@ -158,9 +159,9 @@ func (p Path) ToStringPieces() []string {
 func (s *KubeSpec) ResolveStructure(args *ExplainResourceArgs) {
 	_, gvks := s.Visit(func(path Path, resolved *ResolvedType, circular string) {
 		if circular == "" {
-			fmt.Printf("%+v -- %+v\n", path.ToStringPieces(), resolved)
+			logrus.Debugf("%+v -- %+v\n", path.ToStringPieces(), resolved)
 		} else {
-			fmt.Printf("%+v\n  CIRCULAR %s\n", path.ToStringPieces(), circular)
+			logrus.Debugf("%+v\n  CIRCULAR %s\n", path.ToStringPieces(), circular)
 		}
 	})
 	resources := set.NewSet(args.TypeNames)
@@ -183,26 +184,79 @@ func (s *KubeSpec) ResolveStructure(args *ExplainResourceArgs) {
 			//json.Print(resolved[name])
 			fmt.Printf("\n\n")
 		case "table":
-			panic("TODO")
+			for gv, kind := range gvks[name] {
+				fmt.Printf("%s %s:\n", gv, name)
+				fmt.Printf("%s\n\n", TableResource(kind, args.Depth))
+			}
 		case "condensed":
 			fmt.Printf("%s:\n", name)
 			for gv, kind := range gvks[name] {
-				fmt.Printf("%s:\n", gv)
-				for _, path := range kind.Paths([]string{name}) {
-					if args.Depth == 0 || len(path.Fst) <= args.Depth {
-						prefix := strings.Repeat("  ", len(path.Fst)-1)
-						typeString := fmt.Sprintf("%s%s", prefix, path.Fst[len(path.Fst)-1])
-						//fmt.Printf("%s\n", strings.Join(path.Fst, "."))
-						fmt.Printf("%-60s    %s\n", typeString, path.Snd)
-					}
-				}
-				//json.Print(resolved[name])
-				fmt.Printf("\n\n")
+				fmt.Printf("%s\n\n", CondensedResource(gv, name, kind, args.Depth))
 			}
 		default:
 			panic(errors.Errorf("invalid output format: %s", args.Format))
 		}
 	}
+}
+
+func TableResource(kind *ResolvedType, depth int) string {
+	tableString := &strings.Builder{}
+	table := tablewriter.NewWriter(tableString)
+	table.SetAutoWrapText(false)
+	table.SetRowLine(true)
+	table.SetAutoMergeCells(true)
+	table.SetColMinWidth(1, 100)
+	table.SetHeader([]string{"Type", "Field"})
+	for _, values := range ExplainResource(kind, []string{}, 0, depth) {
+		table.Append([]string{values[1], values[0]})
+	}
+	table.Render()
+	return tableString.String()
+}
+
+func ExplainResource(obj *ResolvedType, pathContext []string, depth int, maxDepth int) [][2]string {
+	logrus.Debugf("path: %+v", pathContext)
+	if maxDepth > 0 && depth >= maxDepth {
+		return nil
+	}
+
+	path := make([]string, len(pathContext))
+	copy(path, pathContext)
+
+	var out [][2]string
+	if obj.Circular != "" {
+		out = append(out, [2]string{strings.Join(path, "."), obj.Circular})
+	} else if obj.Primitive != "" {
+		out = append(out, [2]string{strings.Join(path, "."), obj.Primitive})
+	} else if obj.Array != nil {
+		out = append(out, [2]string{strings.Join(path, "."), "array"})
+		out = append(out, ExplainResource(obj.Array, append(path, "[]"), depth+1, maxDepth)...)
+	} else if obj.Object != nil {
+		out = append(out, [2]string{strings.Join(path, "."), "object"})
+		for _, fieldName := range slice.Sort(maps.Keys(obj.Object.Properties)) {
+			out = append(out, ExplainResource(obj.Object.Properties[fieldName], append(path, fieldName), depth+1, maxDepth)...)
+		}
+		if obj.Object.AdditionalProperties != nil {
+			out = append(out, ExplainResource(obj.Object.AdditionalProperties, append(path, "additionalProperties"), depth+1, maxDepth)...)
+		}
+	} else if obj.Empty {
+
+	} else {
+		panic(errors.Errorf("invalid ResolvedType: %+v", obj))
+	}
+	return out
+}
+
+func CondensedResource(gv string, name string, kind *ResolvedType, depth int) string {
+	lines := []string{gv + ":"}
+	for _, path := range kind.Paths([]string{name}) {
+		if depth == 0 || len(path.Fst) <= depth {
+			prefix := strings.Repeat("  ", len(path.Fst)-1)
+			typeString := fmt.Sprintf("%s%s", prefix, path.Fst[len(path.Fst)-1])
+			lines = append(lines, fmt.Sprintf("%-60s    %s", typeString, path.Snd))
+		}
+	}
+	return strings.Join(lines, "\n")
 }
 
 //func (s *KubeSpec) ResolveGVKs() {
